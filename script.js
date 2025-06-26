1793cdefd14001cbe50e8b914e5d1013a233c1d6
Argel <argeldan28@gmail.com>
2025-06-26 21:52:44 +0200

    link toggle

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu')
+const links = document.querySelectorAll('.navbar-menu li a');
 
 navbarToggle.addEventListener('click', () => {
   navbarToggle.classList.toggle('active');
   navbarMenu.classList.toggle('active');
+})
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    links.forEach(l => l.classList.remove('active'));
+
+    link.classList.add('active');
+
+    navbarMenu.classList.remove('active');
+    navbarToggle.classList.remove('active');
+  })
 })
